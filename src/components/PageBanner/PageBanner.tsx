dbbf4a7e8249dc5e17d5845d162074c22857b162
Sirawit Arsanok <beforever369@gmail.com>
2024-12-23 02:10:50 +0700

    image path

--- src/components/PageBanner/PageBanner.tsx
+++ src/components/PageBanner/PageBanner.tsx
@@ -40,7 +40,7 @@ export const PageBanner: React.FC = ()=> {
                     </div>
                     <div className="relative rounded-full top-0 bg-center">
                         <motion.img 
-                            src="public\images\IMG_7882_crop.JPG" 
+                            src="/public/images/IMG_7882_crop.JPG" 
                             className="object-cover rounded-full w-48 h-48 md:w-96 md:h-96"
                             initial={{ scale: 1 }}
                             animate={{ scale: [1, 1.1, 1] }}
